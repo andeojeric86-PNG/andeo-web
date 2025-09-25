223bcc0ee52e436c917ac74acfed7dd32edbfbb6
andeojeric86-PNG <andeojeric86@gmail.com>
2025-09-25 12:00:45 +0800

    created login form

---
+++ Flowbite/login.js
@@ -0,0 +1,11 @@
+function login(){
+    console.log("You enter the email: " + email)
+    console.log("You enter the password: " + password)
+
+    if (email==="Andeojeric86@gmail.com"){
+        console.log("Your email is correct.");
+    }else{
+        console.log("Your email is incorrect.");
+        document.getElementById 
+    }
+} 
