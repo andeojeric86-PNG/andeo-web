a5abc94f59af27e5826f73093018c8a53afec67a
aivan <andeojeric86@gmail.com>
2025-10-02 11:54:22 +0800

    That"s not done

--- Flowbite/login.js
+++ Flowbite/login.js
@@ -1,11 +1,26 @@
+//Andeo, Jeric ID
 function login(){
     console.log("You enter the email: " + email)
     console.log("You enter the password: " + password)
 
     if (email==="Andeojeric86@gmail.com"){
         console.log("Your email is correct.");
-    }else{
+        document.getElementById("success-msg").innerHTML = "Your email is corrrect";
+   
+    }else(email==="Andeojeric86@gamil.com")
+    {
         console.log("Your email is incorrect.");
-        document.getElementById 
+        document.getElementById("error-msg").innerHTML = "Your email is incorrect";
     }
+
+    if(password==="Jericandeo@080505"){
+            console.log("Your password is correct");
+
+        }else{
+          console.log("Your password is incorrect");
+            document.getElementById("error-msg").innerHTML = "Your password is incorrect";  
+        }
+           
+    }
+
 } 
